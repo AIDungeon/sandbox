c930972735838c56ea9a1c48bc0bafb1c4d2cd18
Rusty Lopez <rustylopez@gmail.com>
2020-08-08 16:57:56 -0600

    Lost an hour learning how to react components kinda

--- LifeGrid.js
+++ LifeGrid.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 
 const Player = function(id, color) {
@@ -11,61 +11,79 @@ const players = [
 ];
 
 
-const LifeGrid = (gameState) => {
+class LifeGrid  extends React.Component {
 
- 
+  constructor(props) {
+    super(props)
 
-    let grid = [];
+    this.state = {
+      grid: []
+    }
+  }
+
+
     
     
-    let selectCell = (lifeGridColumn, lifeGridRow) => {
+  selectCell(lifeGridColumn, lifeGridRow){
 
       console.log("Selected (x:"+lifeGridColumn+",y:"+lifeGridRow+")");
 
-      grid[colIdx][rowIdx] = gameState.getActivePlayer();
-      gameState.setNextPlayerAsActive();  
+      // this.grid[colIdx][rowIdx] = gameState.getActivePlayer();
+      // this.gameState.setNextPlayerAsActive();  
      
     }
 
-    let componentDidMount = () => {
-      grid = [props.columns];
+    componentDidMount() {
+  
+      let testLabel = "(x:"+this.props.columns+",y:"+this.props.rows+")";
+      console.log("Component mounted with Selected " + testLabel);
+      let replacementGrid = [this.props.columns];
 
   
-      for (let colIdx = 0; colIdx < props.columns; colIdx++) {
-          grid[colIdx] = [props.rows];
-          for (let rowIdx = 0; rowIdx < props.rows; rowIdx++) {
-            grid[colIdx][rowIdx] = null;
+      for (let colIdx = 0; colIdx < this.props.columns; colIdx++) {
+          replacementGrid[colIdx] = [this.props.rows.length];
+          for (let rowIdx = 0; rowIdx < this.props.rows; rowIdx++) {
+            replacementGrid[colIdx][rowIdx] = testLabel;
   
           }
       }   
+      this.setState({grid: replacementGrid})
     }
 
-    return (
-        <div class="lifeGrid">
-          {
-          grid.map(colIdx => {
-              (
-                <div class="lifeGridColumn">
-                    {
-                      grid[colIdx].map( rowIdx => {
-                        (
-                          <div class="lifeGridRow">
-                          <Button
-                            onPress={this.selectCell(lifeGridColumn, lifeGridRow)}
-                            title={"Cell: (" + lifeGridColumn + ", " + lifeGridRow + ")"}
-                            color="#AAAAAA"
-                          />
-                          </div>
-                        )
+    render () {
+        return (
+        <div className="lifeGrid">
+          TEST {this.state.grid[0]}
+          { 
+          
+             this.state.grid.map((rows, colIdx) => {
+                (
+                  <div class="lifeGridColumn">
+                      {rows}
+                      {
+                        rows.map( (rowVal, rowIdx) => {
+                          (
+                            <div className="lifeGridRow">
+                              TEST
+                            <Button
+                              onPress={() => {this.selectCell(colIdx, rowIdx)}}
+                              title={"Cell: (" + colIdx + ", " + rowIdx + ")"}
+                              color="#AAAAAA"
+                            />
+                            </div>
+                          )
 
-                      })
-                    }
-                 </div> 
-              )
-              
-          })}
+                        })
+                      } 
+                  </div> 
+                )
+                
+            })
+          }
+
         </div>
-    )
+      )
+    }
 }
 
 export default LifeGrid

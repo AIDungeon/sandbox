fbfd2d37f1638b089c32348c40b91badbcd9ed9d
MaggieFero <maggiefero@gmail.com>
2020-08-12 15:03:11 -0700

    Fixed comments so react ignores them

--- TicTacToe.js
+++ TicTacToe.js
@@ -26,11 +26,11 @@ const TicTacToe = () => {
       <Button>Player X</Button>
       <Button>Player O</Button>
     </div>
-    //TODO: Create a player variable and a method to set it based on clicking "Player X" or "Player O" buttons
+    {/*TODO: Create a player variable and a method to set it based on clicking "Player X" or "Player O" buttons*/}
     <div className={classes.root}>
-      <Grid container spacing={0}>//TODO: Make each grid cell a button with a label of the value variable (whitespace, X, or O) and hooks to a "place letter" method
+      <Grid container spacing={0}>{/*//TODO: Make each grid cell a button with a label of the value variable (whitespace, X, or O) and hooks to a "place letter" method*/}
         <Grid item xs={4}>
-          <Paper className={classes.paper}>xs=3</Paper>//TODO: replace this and all other grid cell values with variables A1-C3
+          <Paper className={classes.paper}>xs=3</Paper>{/*TODO: replace this and all other grid cell values with variables A1-C3*/}
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>xs=3</Paper>
@@ -59,8 +59,8 @@ const TicTacToe = () => {
       </Grid>
       <Text> Tic tac toe is a fun, simple puzzle game played by two players. The game is played on an empty 3x3 grid. Each player takes turns to place their marker (either an X or an O) in an empty cell on the board. The first player to get three of their markers in a straight line in any direction wins. If all cells on the grid are filled and no player has a straight line of three markers then the game is a draw. For more information on the game you can <a href="https://en.wikipedia.org/wiki/Tic-tac-toe"> read more on Wikipedia.</a> </Text>
     </div>
-    <div><Button>Start New Game</Button></div>   //TODO: Set the hook to the method for clearing the board
-   //TODO: Javascript method that iterates over the grid and replaces all values with a whitespace character
+    <div><Button>Start New Game</Button></div>   {/*//TODO: Set the hook to the method for clearing the board*/}
+   {/*//TODO: Javascript method that iterates over the grid and replaces all values with a whitespace character
    //TODO: Place letter method that takes the current cell label variable and sets the value of the variable to the current playerID variable when called
    //TODO: Add a check to the end of the place letter method for a draw: if A1-C3 are all equal to X or O, it's a draw 
     //(NB: Not checking for blanks because equivalency on whitespace characters in JS can cause problems) 
@@ -68,7 +68,7 @@ const TicTacToe = () => {
   //TODO: Add a check to the end of the place letter method for vertical win conditions (if (A1=B1 && B1=C1)|| etc)
   //TODO: Make a single "check for win conditions" method that calls both the horizontal and vertical checks
   //TODO: Add a diagonal win condition check to the "check for win conditions" method (if (A1=B2 && B2=C3) OR (C1=B2 && B2=A3))
- //TODO: Add a state check after the "check draw" and "check win" methods to see if the game is over, and show a message if it is
+   //TODO: Add a state check after the "check draw" and "check win" methods to see if the game is over, and show a message if it is*/}
       </View>)
 }
 

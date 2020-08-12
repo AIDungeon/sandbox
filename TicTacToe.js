ce82c877706607f7f552e87c34a3c8a9c02b5ab8
MaggieFero <maggiefero@gmail.com>
2020-08-12 14:26:01 -0700

    Added more detailed instructions at the bottom

--- TicTacToe.js
+++ TicTacToe.js
@@ -21,7 +21,7 @@ const TicTacToe = () => {
   const classes = useStyles();  
   return (
       <View>
-        <Text>Click "Player X" or "Player O" to choose your letter, then click a grid cell to play tic-tac-toe!</Text>
+        <Text>Click "Player X" or "Player O" to choose your letter, then click a grid cell to play tic-tac-toe! For more info about how to play, check out the bottom of this page.</Text>
     //TODO: Add a div with two buttons for players labeled "Player X" and "Player O"
     //TODO: Create a player variable and a method to set it based on clicking "Player X" or "Player O" buttons
     <div className={classes.root}>
@@ -54,6 +54,12 @@ const TicTacToe = () => {
           <Paper className={classes.paper}>xs=3</Paper>
         </Grid>
       </Grid>
+      <Text>Tic tac toe is a fun, simple puzzle game played by two players. 
+        The game is played on an empty 3x3 grid. 
+        Each player takes turns to place their marker (either an X or an O) in an empty cell on the board. 
+        The first player to get three of their markers in a straight line in any direction wins. 
+        If all cells on the grid are filled and no player has a straight line of three markers then the game is a draw. 
+        For more information on the game you can <Link to="https://en.wikipedia.org/wiki/Tic-tac-toe"> read more on Wikipedia.</Link> </Text>
     </div>
    //TODO: Add another div with a button labeled "Start New Game" and set the hooks to the method for clearing the board
    //TODO: Javascript method that iterates over the grid and replaces all values with a whitespace character
